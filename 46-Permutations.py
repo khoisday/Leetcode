335a6d0eca8e4589981921d812e931c604647408
khoisday <86087565+khoisday@users.noreply.github.com>
2025-01-03 23:50:08 +0700

    [46] [Time Beats: 59.75%] [Memory Beats: 12.37%] - LeetPush

---
+++ 46-Permutations.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def permute(self, nums):
+        \\\
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        \\\
+        res = []
+    
+        def dfs(nums, path):
+            if not nums:
+                res.append(path)
+                return
+            
+            for i in range(len(nums)):
+                dfs(nums[:i] + nums[i+1:], path + [nums[i]])
+        
+        dfs(nums, [])
+        return res
